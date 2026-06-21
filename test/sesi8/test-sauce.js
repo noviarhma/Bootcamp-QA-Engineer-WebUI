814174e63d0e7c49803bb4fcccb00ace67caa620
noviarhma <noviarahmaazzh@gmail.com>
2026-06-21 13:50:50 +0700

    First commit

---
+++ test/sesi8/test-sauce.js
@@ -0,0 +1,44 @@
+//import driver and module
+import {Builder, By, until} from 'selenium-webdriver';
+import assert from 'assert';
+import { expect } from 'chai';
+
+describe('Google Search Test', function() {
+    let driver;
+
+    it('Visit SauceDemo dan cek page title', async function () {
+        //Open website url
+        driver = await new Builder().forBrowser('firefox').build();
+        await driver.get('https://www.saucedemo.com/');
+
+        //Login form
+        let inputUsername = await driver.findElement(By.css('[data-test="username"]'))
+        let inputPassword = await driver.findElement(By.xpath('//*[@id="password"]'))
+        let buttonLogin = await driver.findElement(By.className('submit-button btn_action'))
+        await inputUsername.sendKeys('standard_user')
+        await inputPassword.sendKeys('secret_sauce')
+        await buttonLogin.click()
+
+        //Validasi
+        const buttonChart = await driver.findElement(By.className('shopping_cart_container'))
+        const isDisplayed = await buttonChart.isDisplayed();
+
+        expect(isDisplayed).to.be.true;
+
+        await driver.sleep(1700);
+    });
+
+    it('Sorting Data Desc', async function () {
+        //Filter Sorting Data
+        let dropdownSort = await driver.findElement(By.xpath('//*[@id="header_container"]/div[2]/div/span/select'))
+        await dropdownSort.click()
+        let optionFilter = await driver.findElement(By.xpath('//*[@id="header_container"]/div[2]/div/span/select/option[2]'))
+        await optionFilter.click();
+
+        await driver.sleep(1700);
+
+
+        //close browser
+        await driver.quit();
+    })
+});
